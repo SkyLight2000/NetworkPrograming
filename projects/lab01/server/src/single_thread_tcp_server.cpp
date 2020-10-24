acf573e9c76817ab67c174cee07e6c2e5611cbdf
sky_harley_light <sky.harley.light@gmail.com>
2020-10-24 16:47:26 +0300

    Lab1

---
+++ projects/lab01/server/src/single_thread_tcp_server.cpp
@@ -0,0 +1,40 @@
+#include "single_thread_tcp_server.h"
+
+unsigned int HashFAQ6(const char * str);
+
+void handle_connection(SOCKET socket, sockaddr_in* addr) {
+    char* str_in_addr = inet_ntoa(addr->sin_addr);
+    printf("[%s]>>%s\n", str_in_addr, "Establish new connection");
+
+    char buffer[256] = "";
+    FILE* fd = fopen("Test.txt", "wb");
+    int rc;
+    rc = recv(socket, (char*)&buffer, sizeof(buffer), 0);
+    fwrite(&buffer, 1, rc, fd);
+
+    fclose(fd);
+    close_socket(socket);
+    
+    printf("Received: %s\n", buffer);
+    printf("Hash: %u\n", HashFAQ6(buffer));
+    printf("[%s]>>%s", str_in_addr, "Close incomming connection\n");
+}
+
+unsigned int HashFAQ6(const char * str)
+{
+
+    unsigned int hash = 0;
+
+    for (; *str; str++)
+    {
+        hash += (unsigned char)(*str);
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
+    }
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+
+    return hash;
+
+}
